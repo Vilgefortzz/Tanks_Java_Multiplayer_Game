6c309878dc7add26278a9d33d1f76fdfff71dbe5
Greg <greghause123@gmail.com>
2016-03-07 20:07:14 +0100

    Drobne poprawki. Dodanie harmonogramu prac.

--- README.md
+++ README.md
@@ -1,2 +1,157 @@
 # TanksProject
-Play as a tank and destroy others
+
+Autor: Grzegorz Klimek Gr. Lab. 4 rok II IS WIMIiP
+--------------------------------------------------
+
+Opis projektu: 
+
+Projekt ten przewiduje stworzenie gry, w której użytkownicy będą się rejestrować w bazie danych, 
+podając tzw. nick gracza. Później muszą się zalogować - bez tego niemożliwe jest dołączenie do gry.
+Statystyki wszystkich zarejestrowanych użytkowników będą dostępne w menu.
+Następnie taki użytkownik może wejść do gry dostając do dyspozycji czołg wygenerowany na mapie. 
+Gracz porusza się czołgiem a jego zadanie polega na zniszczeniu wrogich czołgów(innych graczy, którzy 
+połączą sie z serwerem i wejdą do gry).
+
+---------------------------------------------------------------------------------------------------------------------
+
+Harmonogram:
+
+
+[1] Okienka tworzone przy pomocy biblioteki graficznej Swing
+
+[1.1] Stworzenie klasy obsługującej główne okno programu (klasa GUI)
+
+	-> stworzenie okna, odpowiednie ustawienie go na ekranie, nazwanie itp.
+	-> stworzenie panelu odpowiedzialnego za ustawienie tła aplikacji (klasa MyPanel)
+	-> ustawienie icona okna aplikacji
+
+[1.2] Tworzenie GUI + oprogramowanie przycisków
+
+	-> utworzenie menu głównego aplikacji
+	-> stworzenie buttonów, napisów, buttonów wyboru
+	-> ustawienie głównych opcji okna
+	-> działanie bez konieczności otwierania nowych okien - wszystko w jednym oknie
+	-> dodanie akcji do przycisków po kliknięciu myszą - dotyczy menu
+
+[1.3] Wygenerowanie mapy oraz stworzenie modelu czołgu
+
+	-> stworzenie mapy przy pomocy odpowiedniej klasy
+	-> stworzenie czołgu przy pomocy odpowiedniej klasy
+	-> sterowanie czołgiem
+
+[1.4] Stworzenie dodatkowych okien interfejsu
+
+	-> utworzenie interfejsu serwera
+
+[1.5] Wygląd i zachowanie przeciwnych czołgów
+
+	-> nad wrogim czołgiem wyświetlany jest pasek zdrowia, który maleje przy otrzymywaniu obrażeń
+	-> zachowanie czołgów - utrata zdrowia, sprawdzanie czy został zniszczony, jeśli tak to usuwanie go z mapy
+
+---------------------------------------------------------------------------------------------------------------------
+
+[2] Zapis i odczyt plików
+
+[2.1] Zapis do pliku wszystkich zarejestrowanych użytkowników w formie listy
+	
+	-> forma listy:
+		LICZBA GRACZY: ...
+		1) Greg
+		2) Stefan
+		...
+
+[2.2] Zapis do pliku statystyk konkretnego gracza z danego dnia, kiedy grał
+	
+	-> forma listy:
+		Statystyki z dnia 08.09.2016
+		GRACZ: login
+		Liczba zniszczonych czołgów: ...
+		Zostałeś zniszczony : ... razy
+		
+[2.3] Plik z logami na klient (klient)
+
+[2.4] Plik z logami serwera (serwer)
+
+---------------------------------------------------------------------------------------------------------------------
+
+[3] Współbieżność: wątki, operacje atomowe, itp.
+
+[3.1] Utworzenie głównego wątku programu
+
+	-> obsługa zapisu do pliku
+
+[3.2] Utworzenie wątku liczącego zniszczone czołgi i bycie zniszczonym
+
+[3.3] Ruch gracza + kolizje
+
+[3.4] Utworzenie wątku serwera
+
+---------------------------------------------------------------------------------------------------------------------
+
+[4] Bazy danych: JDBC dla: MySQL
+
+[4.1] Podstawowa konfiguracja JBDC
+
+[4.2] Obsługa bazy danych (aplikacja serwera)
+
+	-> łączenie z bazą danych
+	-> tworzenie tabel
+	-> wstawianie danych do bazy
+	-> wczytywanie danych z bazy do listy
+	-> zamykanie połączenia
+
+[4.3] Utworzenie klasy DataToBase (aplikacja serwera)
+	-> zbiór zmiennych przechowujących dane do zapisu/odczytu z bazy danych
+	-> zbiór funkcji wczytujących wartości (get)
+	-> zbiór funkcji wstawiających wartości (set)
+	-> funkcja wyświetlająca
+
+[4.4] Aktualizowanie bazy danych
+
+---------------------------------------------------------------------------------------------------------------------
+
+[5] Komunikacja sieciowa poprzez sockety
+
+[5.1] Aplikacja server
+
+	-> oczekiwanie na połączenie
+	-> odbieranie informacji od klienta
+	-> wykonanie operacji w zależności od flagi przekazanej przez główną aplikację.
+
+[5.2] Aplikacja client
+
+	-> łączenie się z serwerem
+	-> wysyłanie komunikatów do serwera i oczekiwanie na odpowiednią odpowiedź w zależności od wysłanej flagi.
+	-> wykonanie odpowiedniej operacji w zależności od wysłanej informacji zwrotnej od serwera.
+
+[5.3] Ustawienie połączenia wielu klientów poprzez serwer
+
+[5.4] Połączenie za pomocą adresu IP
+
+---------------------------------------------------------------------------------------------------------------------
+
+[6] Zaproponowane przez studenta
+
+[6.1] Wzorzec projektowy - singleton
+
+[6.2] Grafika
+
+	-> dodanie odpowiednich grafik związanych z tematyką gry
+	-> utworzenie przejrzystego menu głównego aplikacji
+	-> zmiana stylu buttonów na przyjemny dla wzroku
+	
+[6.3] Język interfejsu
+
+	-> zmiana języka w grze (Angielski lub Polski) wybierane w menu głównym w opcjach gry
+
+[6.4] Połączenie z zewnętrzną bazą danych
+
+---------------------------------------------------------------------------------------------------------------------
+
+[Legenda]
+
+[1] - poruszane tematy
+[1.1] - zadania
+[->] - podzadania
+
+---------------------------------------------------------------------------------------------------------------------
--- src/main/gui/views/GUI.java
+++ src/main/gui/views/GUI.java
@@ -9,8 +9,6 @@ package main.gui.views;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.IOException;
 
 public class GUI extends JFrame {
@@ -20,9 +18,11 @@ public class GUI extends JFrame {
 
     private MenuPanel menuPanel;
 
+    // przyciski do menu głównego
+
     private JLabel title;
     private JLabel author;
-    private JButton startGameBtn;
+    private JButton playGameBtn;
     private JButton optionsBtn;
     private JButton helpBtn;
     private JButton exitBtn;
@@ -34,7 +34,6 @@ public class GUI extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(WINDOW_WIDTH,WINDOW_HEIGHT);
         setLocationRelativeTo(null);
-        setResizable(false);
         setVisible(true);
 
         // Ustawienie ikonki aplikacji
@@ -100,16 +99,18 @@ public class GUI extends JFrame {
 
         box.add(Box.createVerticalStrut(20));
 
-        startGameBtn = new JButton("Start game");
-        startGameBtn.setForeground(Color.WHITE);
-        startGameBtn.setBackground(Color.BLACK);
-        box.add(startGameBtn);
+        playGameBtn = new JButton("Play");
+        playGameBtn.setForeground(Color.WHITE);
+        playGameBtn.setBackground(Color.BLACK);
+        playGameBtn.setFont(new Font("Arial", Font.PLAIN, 20));
+        box.add(playGameBtn);
 
         box.add(Box.createVerticalStrut(6));
 
         optionsBtn = new JButton("Options");
         optionsBtn.setForeground(Color.WHITE);
         optionsBtn.setBackground(Color.BLACK);
+        optionsBtn.setFont(new Font("Arial", Font.PLAIN, 20));
         box.add(optionsBtn);
 
         box.add(Box.createVerticalStrut(6));
@@ -117,6 +118,7 @@ public class GUI extends JFrame {
         helpBtn = new JButton("Help");
         helpBtn.setForeground(Color.WHITE);
         helpBtn.setBackground(Color.BLACK);
+        helpBtn.setFont(new Font("Arial", Font.PLAIN, 20));
         box.add(helpBtn);
 
         box.add(Box.createVerticalStrut(6));
@@ -124,7 +126,49 @@ public class GUI extends JFrame {
         exitBtn = new JButton("Exit");
         exitBtn.setForeground(Color.WHITE);
         exitBtn.setBackground(Color.BLACK);
+        exitBtn.setFont(new Font("Arial", Font.PLAIN, 20));
         box.add(exitBtn);
+
+        return box;
+    }
+
+    private Box createPlayTheGameMenu(){
+        Box box = Box.createVerticalBox();
+        box.add(Box.createVerticalStrut(20));
+
+        JLabel playRoom = new JLabel("PLAYROOM");
+        playRoom.setForeground(Color.ORANGE);
+        playRoom.setFont(new Font("Courier New", Font.ITALIC, 75));
+        box.add(playRoom);
+
+        box.add(Box.createVerticalStrut(6));
+
+        JButton signUpBtn = new JButton("Sign up");
+        signUpBtn.setForeground(Color.WHITE);
+        signUpBtn.setBackground(Color.BLACK);
+        signUpBtn.setFont(new Font("Arial", Font.PLAIN, 20));
+        box.add(signUpBtn);
+
+        box.add(Box.createVerticalStrut(6));
+
+        JButton signInBtn = new JButton("Sign in");
+        signInBtn.setForeground(Color.WHITE);
+        signInBtn.setBackground(Color.BLACK);
+        signInBtn.setFont(new Font("Arial", Font.PLAIN, 20));
+        box.add(signInBtn);
+
+        box.add(Box.createVerticalStrut(6));
+
+        JButton statusBtn = new JButton("Status"); // status o tym czy jesteśmy zalogowani oraz jako kto
+        statusBtn.setForeground(Color.WHITE);
+        statusBtn.setBackground(Color.BLACK);
+        statusBtn.setFont(new Font("Arial", Font.PLAIN, 20));
+        box.add(statusBtn);
+
+        box.add(Box.createVerticalStrut(6));
+
+        backBtn = backButton();
+        box.add(backBtn);
 
         return box;
     }
@@ -150,6 +194,7 @@ public class GUI extends JFrame {
 
         JRadioButton choose2 = new JRadioButton("English");
         choose2.setFont(new Font("Courier New", Font.ITALIC, 45));
+        choose2.setSelected(true);
         box.add(choose2);
 
         // Grupowanie aby możliwe było zaznaczenie z wykluczeniem drugiego wyboru
@@ -175,7 +220,7 @@ public class GUI extends JFrame {
         JLabel helpText = new JLabel("<html>You are driving a tank. Your goal is to eliminate " +
                 "other tanks.<br><br>1) Sign up entering your login and password" +
                 "<br><br>2) After registration sign in" +
-                "<br><br>3) Now you can join to the game<br><br>Your stats are remembered.<br><br>To move use:<br>W - up<br>A - left" +
+                "<br><br>3) Now you can join to the game<br><br>Your stats are remembered.<br><br>To move use:<br><br>W - up<br>A - left" +
                 "<br>D - right<br>S - down" + "<br>Space - Attack<br><br>Gl and Hf</html>");
         helpText.setFont(new Font("Courier New", Font.ITALIC, 26));
         helpText.setForeground(Color.WHITE);
@@ -191,9 +236,18 @@ public class GUI extends JFrame {
 
     private void createActions(Box box){
 
-        startGameBtn.addActionListener(e -> {
+        playGameBtn.addActionListener(e -> {
             box.setVisible(false);
-            // TODO Rejestracja graczy + już ta właściwa częśc czyli wygenerowanie planszy, grywalność itp.
+            menuPanel.remove(box);
+            Box boxPlayTheGameMenu = createPlayTheGameMenu();
+            menuPanel.add(boxPlayTheGameMenu);
+
+            backBtn.addActionListener(e1 -> {
+                boxPlayTheGameMenu.setVisible(false);
+                menuPanel.remove(boxPlayTheGameMenu);
+                menuPanel.add(box);
+                box.setVisible(true);
+            });
         });
 
         optionsBtn.addActionListener(e -> {
@@ -231,6 +285,7 @@ public class GUI extends JFrame {
         JButton backBtn = new JButton("Back");
         backBtn.setForeground(Color.WHITE);
         backBtn.setBackground(Color.BLACK);
+        backBtn.setFont(new Font("Arial", Font.PLAIN, 20));
         return  backBtn;
     }
 
